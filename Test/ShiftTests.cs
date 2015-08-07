ccb06b9d1d296a3c85bf6ac7e8771fea7ebc5904
Jonathan Yaniv <johniy@me.com>
2015-08-08 00:21:34 +0300

    * ShiftTests.cs: Added 5 tests to check CompareTo and Equals   methods.
    * Test.csproj:
    * packages.config:

---
+++ Test/ShiftTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using ShifterEngine;
+
+
+namespace Test {
+
+	[TestFixture()]
+	public class ShiftTests {
+		[Test()]
+		public void TestEquals() {
+			var shift1 = new Shift(new DateTime(2015, 8, 8, 9, 10, 0), new TimeSpan(10, 12, 0));
+			var shift2 = new Shift(new DateTime(2015, 8, 8, 9, 10, 0), new DateTime(2015, 8, 8, 19, 22, 0));
+			Assert.AreEqual(shift1, shift2, string.Format("Shift1: {0}\nShift2: {1}", shift1, shift2));
+		}
+
+		[Test()]
+		public void TestEquals2() {
+			var shift1 = new Shift(new DateTime(2015, 8, 8, 9, 10, 0), new TimeSpan(10, 12, 0));
+			var shift2 = new Shift(new DateTime(2015, 8, 8, 9, 10, 0), new DateTime(2015, 8, 8, 19, 22, 1));
+			Assert.AreNotEqual(shift1, shift2, string.Format("Shift1: {0}\nShift2: {1}", shift1, shift2));
+		}
+
+		[Test()]
+		public void TestCompare() {
+			var shift1 = new Shift(new DateTime(2015, 8, 8, 9, 10, 0), new TimeSpan(10, 12, 0));
+			var shift2 = new Shift(new DateTime(2015, 8, 8, 9, 10, 0), new DateTime(2015, 8, 8, 19, 22, 1));
+			Assert.AreEqual(-1, shift1.CompareTo(shift2));
+		}
+
+		[Test()]
+		public void TestCompareNull() {
+			var shift1 = new Shift(new DateTime(2015, 8, 8, 9, 10, 0), new TimeSpan(10, 12, 0));
+			Assert.Throws<NullReferenceException>(delegate {
+				shift1.CompareTo(null);
+			});
+		}
+
+		[Test()]
+		public void TestCompareDiffObj() {
+			var shift1 = new Shift(new DateTime(2015, 8, 8, 9, 10, 0), new TimeSpan(10, 12, 0));
+			Assert.Throws<ArgumentException>(delegate {
+				shift1.CompareTo(new object());
+			});
+		}
+	}
+}
+
